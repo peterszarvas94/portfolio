d44188bc70007c510aed30c1b494b9211e475983
Peter Szarvas <contact@peterszarvas.hu>
2025-09-24 17:19:37 +0200

    delete prev

--- README.md
+++
@@ -1,3 +0,0 @@
-# Portfolio
-
-Made using [lytepage](https://github.com/peterszarvas94/lytepage), with Go, templ, tailwind, typescript
--- pkg/config/config.go
+++
@@ -1,8 +0,0 @@
-package config
-
-var (
-	Title      = "lytepage"
-	IndexTitle = "Lite ssg with the power of go + templ"
-	DateLayout = "2006.01.02"
-	TimeLayout = "15:04"
-)
