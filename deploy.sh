d6f85f26331d6dd29c3322b3db8b5fd1717753cc
Peter Szarvas <contact@peterszarvas.hu>
2026-06-15 10:40:36 +0200

    Use mise run build in deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,12 @@ RSYNC_PATH="${RSYNC_PATH:-sudo rsync}"
 
 cd "$ROOT_DIR"
 
-echo "> build"
-templ generate 2>&1 | sed 's/^/  /'
-go run ./cmd 2>&1 | sed 's/^/  /'
+echo "> Running: build"
+mise run build 2>&1 | sed 's/^/  /'
 
-echo "> deploy"
+echo "> Running: deploy"
 rsync -az --delete --progress \
   --rsync-path="$RSYNC_PATH" \
   "${ROOT_DIR}/public/" \
   "${REMOTE_HOST}:${REMOTE_PATH}/" | grep --line-buffered -v '^\s*$' | sed 's/^/  /'
-echo "> https://peterszarvas.hu"
+echo "> Site it deployed: https://peterszarvas.hu"
